d17595e5fd776aa0a8cc6648681cf2d56dd9c002
Martin Jensen <martin.jensen37@gmail.com>
2017-03-14 15:09:02 +0100

    Update Video_cap_RPi.py

--- Video_cap_RPi.py
+++ Video_cap_RPi.py
@@ -16,7 +16,7 @@ cap.resolution = (640, 480)
 cap.framerate = 32
 im = PiRGBArray(cap, size=(640, 480))
 
-for frame in cap.capture_continous(im, format="bgr", use_video_port=True):
+for frame in cap.capture_continuous(im, format="bgr", use_video_port=True):
 
     image = frame.array()
     

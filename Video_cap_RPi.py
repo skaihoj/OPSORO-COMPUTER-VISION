ec6bd3ca133e92c0127a907cd80cea9b0f4a5bbb
Martin Jensen <martin.jensen37@gmail.com>
2017-03-14 15:07:30 +0100

    Update Video_cap_RPi.py

--- Video_cap_RPi.py
+++ Video_cap_RPi.py
@@ -14,7 +14,7 @@ clf = joblib.load("digits_cls.pkl")
 cap = PiCamera()
 cap.resolution = (640, 480)
 cap.framerate = 32
-im = PiRGBArray(camera, size=(640, 480))
+im = PiRGBArray(cap, size=(640, 480))
 
 for frame in cap.capture_continous(im, format="bgr", use_video_port=True):
 
